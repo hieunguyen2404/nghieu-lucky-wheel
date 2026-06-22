ea654b8d1a5626f5b1f4af05b07280b628b4258b
hieunguyen2404 <nguyengiahieupt19l1@gmail.com>
2026-06-22 23:17:29 +0700

    [B] update wheel types 220626

--- types/wheel.ts
+++ types/wheel.ts
@@ -15,33 +15,3 @@ export interface WheelSettings {
   textPosition: number;
   questions: Question[];
 }
-const defaultSettings: WheelSettings = {
-  wheelMode: 2,
-
-  wheelData: ["Lương Viết Hiệu", "Cao Viết Được", "Định Thị Thanh Huyền"],
-
-  spinResults: {},
-
-  spins: 16,
-
-  duration: 19500,
-
-  showGiftSection: true,
-
-  numberFontSize: 0.08,
-
-  nameFontSize: 0.045,
-
-  textPosition: 0.85,
-
-  questions: [
-    {
-      question: "Câu hỏi 1",
-      answer: "Đáp án 1",
-    },
-    {
-      question: "Câu hỏi 2",
-      answer: "Đáp án 2",
-    },
-  ],
-};
